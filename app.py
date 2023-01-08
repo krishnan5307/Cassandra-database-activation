b0cdb4b5d5aa0c44296c75ab79695dc2df7273ef
krishnanunni br <krishnanunni5307@gmail.com>
2023-01-08 22:18:45 +0530

    changed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import sys
-import data_ingestion as pdf
+import data_ingestion 
 import connect_database
 import pip
 import os, sys
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     try:
-        return pdf.get_data_ingestion
+        return data_ingestion.get_data_ingestion().get_data()
     except Exception as e:
         return str(e)
 
--- data_ingestion.py
+++ data_ingestion.py
@@ -1,27 +1,27 @@
+import logging
+import pandas as pd
+import connect_database 
 
-
+CASSANDRA_KEYSPACE = "insurance"
+CASSANDRA_TABLE = "insurance"
 
 class get_data_ingestion:
 
 
     def __init__(self):
-        pass
+        self.connect_database = connect_database
+        
 
-    def get_data(self)-> session
+    def get_data(self):
+        
+        logging.info(f"connction sent to connect.py file")
+        session = self.connect_database().get_db_connection()
+        sql_query = "SELECT * FROM {}.{};".format(CASSANDRA_KEYSPACE, CASSANDRA_TABLE)
+        df = pd.DataFrame()
+        for row in session.execute(sql_query):
+            df = df.append(pd.DataFrame(row, index=[0]))
+        df.to_csv("dataset",mode="w", index=False,header=True)  
+       
 
 
-
-
-
-            #from cassandra database using connction variable to download dataset to dataframe and later to csv
-            download_url = self.data_ingestion_config.dataset_download_url
-            logging.info(f"connction sent to connect_databsae.py file")
-            
-            session = self.database_connection.get_db_connection()
-            
-            sql_query = "SELECT * FROM {}.{};".format(constant.CASSANDRA_KEYSPACE, constant.CASSANDRA_TABLE)
-            df = pd.DataFrame()
-            for row in session.execute(sql_query):
-                    df = df.append(pd.DataFrame(row, index=[0]))
-
-            session.shutdown()
+    
